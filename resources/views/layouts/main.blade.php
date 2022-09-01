d1fba8006b1067413bcaed25a8afc3ff43eaca8d
Dmitry Vystropov <beepper210980@gmail.com>
2022-09-01 19:42:48 +0300

    Добавлен Nav bar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,13 +11,33 @@
 <body>
 <div class="container">
     <div class="row">
-        <nav> <!-- Меню навигации -->
-            <ul> <!-- Создает маркированный список. ТЭГ ul -->
-                <li><a href="{{route('complex.index')}}">Комплексы</a></li> <!-- Элемент маркированного или нумерованного списка. ТЭГ li -->
-                <li><a href="{{route('about.index')}}">О Нас</a></li>
-                <li><a href="{{route('contact.index')}}">Контакты</a></li>
-            </ul>
+
+        <nav class="navbar navbar-expand-lg  bg-light">
+            <div class="container-fluid">
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="{{route('complex.index')}}">Комплексы</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('about.index')}}">О Нас</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('contact.index')}}">Контакты</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
+
+{{--        <nav> <!-- Меню навигации -->--}}
+{{--            <ul> <!-- Создает маркированный список. ТЭГ ul -->--}}
+{{--                <li><a href="{{route('complex.index')}}">Комплексы</a></li> <!-- Элемент маркированного или нумерованного списка. ТЭГ li -->--}}
+{{--                <li><a href="{{route('about.index')}}">О Нас</a></li>--}}
+{{--                <li><a href="{{route('contact.index')}}">Контакты</a></li>--}}
+{{--            </ul>--}}
+{{--        </nav>--}}
     </div>
 
     @yield('content') <!--// указываем имя секции которую надо вставить в этом месте шаблона-->
