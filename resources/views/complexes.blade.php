04df5107effa9e61240130b48d3acd422620a95e
Dmitry Vystropov <beepper210980@gmail.com>
2022-09-01 18:35:23 +0300

    Установка bootstrap

--- resources/views/complexes.blade.php
+++ resources/views/complexes.blade.php
@@ -6,4 +6,28 @@
     @foreach(@$complexes as $complex)
      <div> {{ $complex->NameComplex }} -  {{ $complex->Description }}</div>
     @endforeach
+
+<div>
+    <table class="table table-striped table-dark">
+        <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+        </tr>
+        </thead>
+
+        <tbody>
+        @foreach(@$complexes as $complex)
+        <tr>
+            <th scope="row">{{ @$complex->id  }}</th>
+            <td>{{ $complex->NameComplex }}</td>
+            <td>{{ $complex->Description }}</td>
+        </tr>
+        @endforeach
+        </tbody>
+    </table>
+</div>
+
+
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,11 +5,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <title>Document</title>
 </head>
 <body>
-<div>
-    <div>
+<div class="container">
+    <div class="row">
         <nav> <!-- Меню навигации -->
             <ul> <!-- Создает маркированный список. ТЭГ ul -->
                 <li><a href="{{route('complex.index')}}">Комплексы</a></li> <!-- Элемент маркированного или нумерованного списка. ТЭГ li -->
